5495483054582478a432856bb4e4f2b57b7ad89e
Atharvnasare <nasareatharv2003@gmail.com>
2025-06-06 22:38:02 +0530

    new commit 5

--- eleven.c
+++ eleven.c
@@ -141,20 +141,52 @@
 
 /*Create an Array of size 5 (using calloc) and enter its value from the user*/
 
-int main(){
-    int *ptr;
-    ptr = (int *) calloc (5 , sizeof(int ));
+// int main(){
+//     int *ptr;
+//     ptr = (int *) calloc (5 , sizeof(int ));
 
-    printf("Enter Number (5) = ");
-    for(int i=0 ; i<5 ; i++){
-        scanf("%d" , &ptr[i]);
-    }
+//     printf("Enter Number (5) = ");
+//     for(int i=0 ; i<5 ; i++){
+//         scanf("%d" , &ptr[i]);
+//     }
 
-    //print
+//     //print
 
-    for(int i=0 ; i<5 ; i++){
-        printf("Number %d is %d \n" , i , ptr[i]);
-    }
+//     for(int i=0 ; i<5 ; i++){
+//         printf("Number %d is %d \n" , i , ptr[i]);
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
+// ___________________________________________
+
+/*Allocate memory to store first 5 odd numbers , then reallocate it to store first 6 even number*/
+
+// int main(){
+//     int *ptr;
+//     ptr = (int *) calloc (5 , sizeof(int));
+//     ptr[0] = 1;
+//         ptr[1] = 3;
+//             ptr[2] = 5;
+//                 ptr[3] = 7;
+//                     ptr[4] = 9;
+//     for(int i=0 ; i<5 ; i++){
+//         printf("%d \n" , ptr[i]);
+//     }
+//     printf("\n");
+
+//     ptr = realloc(ptr , 6);
+//     ptr[0] = 2 ;
+//         ptr[1] = 4;
+//             ptr[2] = 6;
+//                 ptr[3] = 8;
+//                     ptr[4] = 10;
+//                         ptr[5] = 12;
+//     for(int i=0 ; i<6 ; i++){
+//         printf("%d \n" , ptr[i]);
+//     }
+
+//     free(ptr);
+// return 0;
+// }
